d0b15d0bccd58db0835bf2695524759e33fe9f40
dima <veryovkin.dmitry@gmail.com>
2024-01-20 14:52:58 +0200

    Version program 3.0

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-# class Student:
-#     group = "C2019"
-#     gender = "Male"
-#     education = "MKA STEP"
-#
-#     def __init__(self):
-#         # print(id(self))
-#         self.name = "Oleg"
-#         self.age = 15
-#
-#
-# st1 = Student()
-# print(st1.name)
-# print(st1.age)
-# # print(id(st1))
-# # st2 = Student()
+class Student:
+    group = "C2019"
+    gender = "Male"
+    education = "MKA STEP"
 
-# print (st1.group)
-# print (st2.group)
+    def __init__(self):
+        # print(id(self))
+        self.name = "Oleg"
+        self.age = 15
+
+
+st1 = Student()
+print(st1.name)
+print(st1.age)
+# print(id(st1))
+# st2 = Student()
+
+print (st1.group)
+print (st2.group)
 
 
